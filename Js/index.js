497dbac59903d916a9bf8eb904beb75bb46afc0e
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-02-18 19:53:46 +0600

    i am so so done

--- Js/index.js
+++ Js/index.js
@@ -43,65 +43,28 @@ document.getElementById('Calculate-btn').addEventListener('click', function () {
     const errorMessage = document.getElementById('error-message');
     const negativeError = document.getElementById('negative-error');
     const errorIncrease = document.getElementById("increase-error");
-    // const totalBalance = document.getElementById('total-balance');
-    // const totalExpenses = document.getElementById('Total-Expenses');
     const totalExpensesAmount = getTotalExpenses();
+
 
     if (newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0) {
         updateTotalCost('Total-Expenses', newInputValue1, newInputValue2, newInputValue3);
         updateBalance(newInputValue1, newInputValue2, newInputValue3, true);
     }
+
     if (newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0 && newInputValue1 < incomeField && newInputValue2 < incomeField && newInputValue3 < incomeField) {
         updateTotalCost('Total-Expenses', newInputValue1, newInputValue2, newInputValue3);
         updateBalance(newInputValue1, newInputValue2, newInputValue3, false);
     }
-
-    else if (newInputValue1 < 0 && newInputValue2 < 0 && newInputValue3 < 0 && incomeField < 0) {
+    else if (newInputValue1 < 0 && newInputValue2 < 0 && newInputValue3 < 0) {
         negativeError.style.display = 'block';
-        // errorMessage.style.display = 'none';
+    }
+
+    if (totalExpensesAmount > incomeField) {
+        errorIncrease.style.display = 'block';
     }
 
     else if (newInputValue1 == String("") && newInputValue2 == String("") && newInputValue3 == String("") && incomeField == String("")) {
         errorMessage.style.display = 'block';
-        // negativeError.style.display = 'none';
     }
-    else if (totalExpensesAmount > incomeField) {
-        errorIncrease.style.display = 'block';
-    }
+
 })
-
-
-
-
-
-
-
-
-function handleError() {
-
-
-    // const negativeError = document.getElementById('negative-error');
-    // const number = event.target.value;
-    // totalBalance > incomeField && totalBalance < 0 && totalExpenses > incomeField
-    // const incomeField = document.getElementById('income-field').value;
-    const totalBalance = document.getElementById('total-balance');
-    // const totalExpenses = document.getElementById('Total-Expenses').innerText;
-    const errorMessage = document.getElementById('error-message');
-    const errorIncrease = document.getElementById("increase-error");
-    //
-    // }
-
-    // handleError();
-    if (totalBalance > incomeField && totalExpenses > incomeField) {
-        // negativeError.style.display = 'block';
-        // incomeField.value = '';
-        // totalBalance.innerText = '';
-        errorIncrease.style.display = 'block';
-    }
-    else {
-        errorMessage.style.display = 'block';
-    }
-
-}
-
-// if (totalBalance < 0 && incomeField == '')
