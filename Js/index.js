92feb416cd270ec063e97caa7939a6f6719f7905
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-02-18 15:56:48 +0600

    make another mistake

--- Js/index.js
+++ Js/index.js
@@ -1,5 +1,4 @@
 //
-
 function updateTotalCost(foodValue, rentValue, clothValue) {
     const foodInputValue = document.getElementById(foodValue);
     const rentInputValue = document.getElementById(rentValue);
@@ -8,7 +7,7 @@ function updateTotalCost(foodValue, rentValue, clothValue) {
     return totalCost;
 }
 
-function getIncomeInput() {
+function getTotalExpenses() {
     const totalExpenses = document.getElementById('Total-Expenses');
     const totalExpensesAmount = totalExpenses.innerText;
     return totalExpensesAmount;
@@ -19,10 +18,9 @@ function updateBalance(foodValue, rentValue, clothValue, money) {
     const totalIncomeField = incomeField.value;
     const totalExpenses = document.getElementById('Total-Expenses');
     const totalBalance = document.getElementById('total-balance');
-    const totalExpensesAmount = getIncomeInput();
+    const totalExpensesAmount = getTotalExpenses();
     if (money == true) {
         totalExpenses.innerText = parseInt(foodValue) + parseInt(rentValue) + parseInt(clothValue);
-        // totalBalance.innerText = parseInt(totalIncomeField) - parseInt(totalValue);
     }
     else {
         totalBalance.innerText = parseInt(totalIncomeField) - parseInt(totalExpensesAmount);
@@ -35,19 +33,77 @@ function getInputValue(inputId1, inputId2, inputId3) {
     inputField.value = '';
     return inputValue;
 }
+
 //
 document.getElementById('Calculate-btn').addEventListener('click', function () {
-    const incomeInputField = document.getElementById(' getInputValue');
     const newInputValue1 = getInputValue('food-field');
     const newInputValue2 = getInputValue('rent-field');
     const newInputValue3 = getInputValue('Cloth-field');
+    const incomeField = document.getElementById('income-field');
+    const errorMessage = document.getElementById('error-message');
+    const negativeError = document.getElementById('negative-error');
+
     // const totalInputValue = newInputValue.value ;
     if (newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0) {
         updateTotalCost('Total-Expenses', newInputValue1, newInputValue2, newInputValue3);
         updateBalance(newInputValue1, newInputValue2, newInputValue3, true);
     }
-    if (newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0 && newInputValue1 < incomeInputField && newInputValue2 < incomeInputField && newInputValue3 < incomeInputField) {
+    if (newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0 && newInputValue1 < incomeField && newInputValue2 < incomeField && newInputValue3 < incomeField) {
         updateBalance(newInputValue1, newInputValue2, newInputValue3, false);
     }
+
+    if (newInputValue1 < 0 && newInputValue2 < 0 && newInputValue3 < 0 && incomeField < 0) {
+        // console.log(alert('its not allowed'))
+        negativeError.style.display = 'block';
+        errorMessage.style.display = 'none';
+
+    }
+
+    else if (newInputValue1 == String("") && newInputValue2 == String("") && newInputValue3 == String("") && incomeField == String("")) {
+        errorMessage.style.display = 'block';
+        // console.log(alert('its not allowed'))
+        negativeError.style.display = 'none';
+    }
+
 })
 
+
+// newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0
+
+// else if (newInputValue1 = newInputValue2 = newInputValue3 < 0) {
+//     errorMessage.style.display = 'block';
+// }
+
+
+
+
+
+
+
+function handleError() {
+
+
+    // const negativeError = document.getElementById('negative-error');
+    // const number = event.target.value;
+    // totalBalance > incomeField && totalBalance < 0 && totalExpenses > incomeField
+    // const incomeField = document.getElementById('income-field').value;
+    const totalBalance = document.getElementById('total-balance');
+    // const totalExpenses = document.getElementById('Total-Expenses').innerText;
+    const errorMessage = document.getElementById('error-message');
+    //
+    // }
+
+    // handleError();
+    if (totalBalance > 0 && totalBalance < incomeField) {
+        // negativeError.style.display = 'block';
+        // incomeField.value = '';
+        // totalBalance.innerText = '';
+        errorMessage.style.display = 'none';
+    }
+    else {
+        errorMessage.style.display = 'block';
+    }
+
+}
+
+// if (totalBalance < 0 && incomeField == '')
