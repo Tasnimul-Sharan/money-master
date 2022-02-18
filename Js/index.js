c79b6b709689000da44e34454c360e6a32122c83
Tasnimul-Shara <tasnimulsharan8962@gmail.com>
2022-02-18 17:50:46 +0600

    negative value essues

--- Js/index.js
+++ Js/index.js
@@ -42,37 +42,34 @@ document.getElementById('Calculate-btn').addEventListener('click', function () {
     const incomeField = document.getElementById('income-field');
     const errorMessage = document.getElementById('error-message');
     const negativeError = document.getElementById('negative-error');
+    const errorIncrease = document.getElementById("increase-error");
+    // const totalBalance = document.getElementById('total-balance');
+    // const totalExpenses = document.getElementById('Total-Expenses');
+    const totalExpensesAmount = getTotalExpenses();
 
-    // const totalInputValue = newInputValue.value ;
     if (newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0) {
         updateTotalCost('Total-Expenses', newInputValue1, newInputValue2, newInputValue3);
         updateBalance(newInputValue1, newInputValue2, newInputValue3, true);
     }
     if (newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0 && newInputValue1 < incomeField && newInputValue2 < incomeField && newInputValue3 < incomeField) {
+        updateTotalCost('Total-Expenses', newInputValue1, newInputValue2, newInputValue3);
         updateBalance(newInputValue1, newInputValue2, newInputValue3, false);
     }
 
-    if (newInputValue1 < 0 && newInputValue2 < 0 && newInputValue3 < 0 && incomeField < 0) {
-        // console.log(alert('its not allowed'))
+    else if (newInputValue1 < 0 && newInputValue2 < 0 && newInputValue3 < 0 && incomeField < 0) {
         negativeError.style.display = 'block';
-        errorMessage.style.display = 'none';
-
+        // errorMessage.style.display = 'none';
     }
 
     else if (newInputValue1 == String("") && newInputValue2 == String("") && newInputValue3 == String("") && incomeField == String("")) {
         errorMessage.style.display = 'block';
-        // console.log(alert('its not allowed'))
-        negativeError.style.display = 'none';
+        // negativeError.style.display = 'none';
     }
-
+    else if (totalExpensesAmount > incomeField) {
+        errorIncrease.style.display = 'block';
+    }
 })
 
-
-// newInputValue1 > 0 && newInputValue2 > 0 && newInputValue3 > 0
-
-// else if (newInputValue1 = newInputValue2 = newInputValue3 < 0) {
-//     errorMessage.style.display = 'block';
-// }
 
 
 
@@ -90,15 +87,16 @@ function handleError() {
     const totalBalance = document.getElementById('total-balance');
     // const totalExpenses = document.getElementById('Total-Expenses').innerText;
     const errorMessage = document.getElementById('error-message');
+    const errorIncrease = document.getElementById("increase-error");
     //
     // }
 
     // handleError();
-    if (totalBalance > 0 && totalBalance < incomeField) {
+    if (totalBalance > incomeField && totalExpenses > incomeField) {
         // negativeError.style.display = 'block';
         // incomeField.value = '';
         // totalBalance.innerText = '';
-        errorMessage.style.display = 'none';
+        errorIncrease.style.display = 'block';
     }
     else {
         errorMessage.style.display = 'block';
